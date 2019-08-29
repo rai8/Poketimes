cb89f4fd1cd55aa9058d6942696e4a527fb44e13
rai8 <hytonne@outlook.com>
2019-08-29 15:54:41 +0300

    fetch data from API

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,25 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
+import Axios from "axios";
 
-function Home() {
-  return (
-    <div className="container">
-      <h4 className="center">Home</h4>
-      <p>
-        React Router is the de-facto React routing library, and it’s one of the
-        most popular projects built on top of React. React at its core is a very
-        simple library, and it does not dictate anything about routing. Routing
-        in a Single Page Application is the way to introduce some features to
-        navigating the app through links, which are expected in normal web
-        applications: The browser should change the URL when you navigate to a
-        different screen Deep linking should work: if you point the browser to a
-        URL, the application should reconstruct the same view that was presented
-        when the URL was generated. The browser back (and forward) button should
-        work like expected. Routing links together your application navigation
-        with the navigation features offered by the browser: the address bar and
-        the navigation buttons.
-      </p>
-    </div>
-  );
+class Home extends Component {
+  state = {
+    posts: []
+  };
+  componentDidMount() {
+    Axios.get("https://jsonplaceholder.typicode.com/posts").then(res => {
+      console.log(res);
+      this.setState({ posts: res.data.slice(0, 10) });
+    });
+  }
+  render() {
+    const { posts } = this.state;
+    const postList = posts.length ? (
+      posts.map(post => {
+        return (
+          <div className="post card" key={post.id}>
+            <div className="card-content">
+              <span className="card-title">{post.title}</span>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        );
+      })
+    ) : (
+      <div className="center">
+        <p>No posts yet</p>
+      </div>
+    );
+    return (
+      <div className="container">
+        <h4 className="center">Home</h4>
+        <p>{postList}</p>
+      </div>
+    );
+  }
 }
-
 export default Home;
