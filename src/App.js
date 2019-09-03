2d2c63cd2fcb371d3adb74491c1ce544005b2414
rai8 <hytonne@outlook.com>
2019-09-03 22:12:48 +0300

    setting up route parameters and use of switch cases

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Contacts from "./components/Contacts";
+import Post from "./components/Post";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Navbar />
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/contact" component={Contacts} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contacts} />
+          <Route path="/:post_id" component={Post} />
+        </Switch>
       </div>
     </BrowserRouter>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Axios from "axios";
+import { Link } from "react-router-dom";
 
 class Home extends Component {
   state = {
@@ -7,7 +8,6 @@ class Home extends Component {
   };
   componentDidMount() {
     Axios.get("https://jsonplaceholder.typicode.com/posts").then(res => {
-      console.log(res);
       this.setState({ posts: res.data.slice(0, 10) });
     });
   }
@@ -18,7 +18,10 @@ class Home extends Component {
         return (
           <div className="post card" key={post.id}>
             <div className="card-content">
-              <span className="card-title">{post.title}</span>
+              <Link to={"/" + post.id}>
+                {" "}
+                <span className="card-title">{post.title}</span>
+              </Link>
               <p>{post.body}</p>
             </div>
           </div>
