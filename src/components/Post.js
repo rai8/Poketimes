ad5b763d2617ef796267662173552ca94f18a6e9
rai8 <hytonne@outlook.com>
2019-09-04 15:43:26 +0300

    mapping states to props

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,21 +1,12 @@
 import React, { Component } from "react";
-import Axios from "axios";
+import { connect } from "react-redux";
 
 class Post extends Component {
-  state = {
-    post: null
-  };
-  componentDidMount() {
-    let id = this.props.match.params.post_id;
-    Axios.get("https://jsonplaceholder.typicode.com/posts/" + id).then(res => {
-      this.setState({ post: res.data });
-    });
-  }
   render() {
-    const post = this.state.post ? (
+    const post = this.props.post ? (
       <div className="post">
-        <h4 className="center">{this.state.post.title}</h4>
-        <p>{this.state.post.body}</p>
+        <h4 className="center">{this.props.post.title}</h4>
+        <p>{this.props.post.body}</p>
       </div>
     ) : (
       <div className="center">Loading post...</div>
@@ -23,4 +14,10 @@ class Post extends Component {
     return <div className="container">{post}</div>;
   }
 }
-export default Post;
+const mapStateToProps = (state, ownProps) => {
+  let id = ownProps.match.params.post_id;
+  return {
+    post: state.posts.find(post => post.id === id)
+  };
+};
+export default connect(mapStateToProps)(Post);
